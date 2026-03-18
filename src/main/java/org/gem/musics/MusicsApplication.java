11ee9188b11c9a6a4c9a702ed9b7f2ef55ed29ce
GABRIEL EHRAT FAGUNDES <gabriel_e_fagundes@estudante.sesisenai.org.br>
2026-03-18 14:22:14 -0300

    feat: Initial project files

---
+++ src/main/java/org/gem/musics/MusicsApplication.java
@@ -0,0 +1,13 @@
+package org.gem.musics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicsApplication.class, args);
+	}
+
+}
