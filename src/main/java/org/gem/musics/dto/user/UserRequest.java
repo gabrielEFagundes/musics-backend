b70a8c737b486e8fa59d5a815f6826ee9c50c514
GABRIEL EHRAT FAGUNDES <gabriel_e_fagundes@estudante.sesisenai.org.br>
2026-03-30 19:35:14 -0300

    hotfix: Fixing nullable user/artist profile pictures

--- src/main/java/org/gem/musics/dto/user/UserRequest.java
+++ src/main/java/org/gem/musics/dto/user/UserRequest.java
@@ -1,4 +1,7 @@
 package org.gem.musics.dto.user;
 
 public record UserRequest(String name, String profilePictureUrl) {
+    public UserRequest{
+        if(profilePictureUrl == null || profilePictureUrl.isEmpty()) profilePictureUrl = "https://images.unsplash.com/vector-1742875355318-00d715aec3e8?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    }
 }
